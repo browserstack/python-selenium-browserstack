691c194ff850d7aa5af84d8f22b1d2e9f00a92b3
Ankit Singh <2501.ankit@gmail.com>
2022-08-12 19:57:17 +0530

    chore: update session name, fix indentation

--- scripts/single.py
+++ scripts/single.py
@@ -14,11 +14,9 @@ URL = os.environ.get("URL") or "https://hub.browserstack.com/wd/hub"
 
 bstack_options = {
     "os" : "OS X",
-    "osVersion" : "Sierra",
-    "buildName" : "Final-Snippet-Test",
-    "sessionName" : "Selenium-4 Python snippet test",
-    "local" : "false",
-    "seleniumVersion" : "4.0.0",
+    "osVersion" : "Monterey",
+    "buildName" : "browserstack-build-1",
+    "sessionName" : "BStack Python sample",
     "userName": BROWSERSTACK_USERNAME,
     "accessKey": BROWSERSTACK_ACCESS_KEY
 }
@@ -32,20 +30,31 @@ try:
     driver.get("https://bstackdemo.com/")
     WebDriverWait(driver, 10).until(EC.title_contains("StackDemo"))
     # Get text of an product - iPhone 12
-    item_on_page =  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/p'))).text
+    item_on_page = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/p'))).text
     # Click the 'Add to cart' button if it is visible
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/div[4]'))).click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, '//*[@id="1"]/div[4]'))).click()
     # Check if the Cart pane is visible
-    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME, "float-cart__content")))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.CLASS_NAME, "float-cart__content")))
     ## Get text of product in cart
-    item_in_cart = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'))).text
+    item_in_cart = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'))).text
     # Verify whether the product (iPhone 12) is added to cart
     if item_on_page == item_in_cart:
-        # Set the status of test as 'passed' or 'failed' based on the condition; if item is added to cart
-        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "iPhone 12 has been successfully added to the cart!"}}')
+        # Set the status of test as 'passed' if item is added to cart
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "iPhone 12 has been successfully added to the cart!"}}')
+    else:
+        # Set the status of test as 'failed' if item is not added to cart
+        driver.execute_script(
+            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "iPhone 12 not added to the cart!"}}')
 except NoSuchElementException:
-    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some elements failed to load"}}')
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some elements failed to load"}}')
 except Exception:
-    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some exception occurred"}}')
+    driver.execute_script(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some exception occurred"}}')
 # Stop the driver
 driver.quit()
