c9b833dad4266cc8f8ec2f1fc32e2c12a49c9f15
Ankit Singh <2501.ankit@gmail.com>
2022-10-10 15:44:41 +0530

    chore: fix import

--- scripts/local.py
+++ scripts/local.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import json
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options as ChromeOptions
