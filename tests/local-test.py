60415b40fa2625e93474c10e82a70a5d6485058c
Ankit Singh <2501.ankit@gmail.com>
2022-11-09 11:15:10 +0530

    update: modify local test with default localIdentifier

--- tests/local-test.py
+++ tests/local-test.py
@@ -15,11 +15,10 @@ driver = webdriver.Remote(
     options=options)
 
 try:
-    driver.get('http://bs-local.com:45691/check')
-    body_text = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'body'))).text
+    driver.get('http://bs-local.com:45454')
+    page_title = driver.title
     # check if local connected successfully
-    if body_text == 'Up and running':
+    if page_title == 'BrowserStack Local':
         # mark test as passed if Local website is accessible
         driver.execute_script(
             'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Local Test ran successfully"}}')
