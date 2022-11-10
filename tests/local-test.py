84a2f47c930b22a73bafe148164ca5536ff9aa1a
Ankit Singh <2501.ankit@gmail.com>
2022-11-10 12:41:46 +0530

    chore: remove unused import

--- tests/local-test.py
+++ tests/local-test.py
@@ -1,8 +1,5 @@
 import json
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 options = ChromeOptions()
